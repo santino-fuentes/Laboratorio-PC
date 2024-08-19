1e2344ecdd0bf99b281830e23105dc78cb8fb424
santino-fuentes <santino.fuentes@est.fi.uncoma.edu.ar>
2024-08-19 15:54:47 -0300

    Add: clase Aeropuerto

---
+++ ProgramacionConcurrente/src/tp_0/ejercicio_2/Aeropuerto.java
@@ -0,0 +1,15 @@
+package tp_0.ejercicio_2;
+
+public class Aeropuerto
+{
+	private Alquiler[] alquileres = Alquiler[100];
+
+	public registrarAlquiler(int unaPosicionEstacionamiento,
+			Avion unAvion,
+			Cliente unCliente,
+			int unaCantidadDias)
+	{
+		// Qué pasa acá?
+		// En el dominio pide fecha inicio y fin y acá pide cantidad de días
+	}
+}
