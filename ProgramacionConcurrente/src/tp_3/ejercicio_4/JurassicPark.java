bdb327b0a9e5405210ba2e1eedcee27b2ce95bf8
santinux <santinofuentes24.sf@gmail.com>
2024-09-22 22:58:32 -0300

    Add: tp_3.ejercicio_4

---
+++ ProgramacionConcurrente/src/tp_3/ejercicio_4/JurassicPark.java
@@ -0,0 +1,16 @@
+package tp_3.ejercicio_4;
+
+/**
+ *
+ * @author sang
+ */
+public class JurassicPark
+{
+        /*
+        En un sistema de reserva de espacios en un parque temático en línea,
+        los visitantes pueden seleccionar y reservar áreas específicas para
+        actividades y entretenimiento.
+        Cada área tiene un número limitado de espacios disponibles.
+        */
+        
+}
---
+++ ProgramacionConcurrente/src/tp_3/ejercicio_4/Visitante.java
@@ -0,0 +1,9 @@
+package tp_3.ejercicio_4;
+
+/**
+ *
+ * @author sang
+ */
+public class Visitante
+{
+}
